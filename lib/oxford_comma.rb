7ee56e9a851488a54e2ca0238b921f68637e389a
Andrada Lorena Popa <andrada.lorena.popa@gmail.com>
2019-10-17 16:41:18 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,8 +4,8 @@ def oxford_comma(array)
     string<< array.join(",")
     elsif array.size==2 
     string<< array.join(" and ")
-    elsif array.size>==3 
-    array[-1]="and"+ array[-1]
+    elsif array.size>=3 
+    array[-1]="and "+ array[-1]
       string<< array.join(", ")
   end
   string
