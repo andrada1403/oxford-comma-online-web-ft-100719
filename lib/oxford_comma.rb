063be21f3e17f9c6cfcbd31134f01cd5f377d920
Andrada Lorena Popa <andrada.lorena.popa@gmail.com>
2019-10-17 16:37:49 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  string=""
+  if array.size==1 
+    string<< array.join(",")
+    elsif array.size==2 
+    string<< array.join(" and ")
+    elsif array.size>==3 
+    array[-1]="and"+ array[-1]
+      string<< array.join(", ")
+  end
+  string
 end
